ef359f773883a3d0eb1edc9a4fabcb855033d4d9
ayoline <ayo@outlook.com.br>
2022-09-07 11:48:58 -0300

    add fresh and shuffle deck functionality

---
+++ frontend/src/pages/game/script.js
@@ -0,0 +1,5 @@
+import Deck from '../../utils/deck.js';
+
+const deck = new Deck();
+// deck.shuffle();
+console.log(deck.cards);
---
+++ frontend/src/utils/deck.js
@@ -0,0 +1,44 @@
+const SUITS = ['b', 'g', 'r', 'y'];
+const VALUES = ['t', '+2', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+export default class Deck {
+  constructor(cards = freshDeck()) {
+    this.cards = cards;
+  }
+
+  get numberofCards() {
+    return this.cards.length;
+  }
+
+  shuffle() {
+    for (let i = this.numberofCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1));
+      const oldValue = this.cards[newIndex];
+      this.cards[newIndex] = this.cards[i];
+      this.cards[i] = oldValue;
+    }
+  }
+}
+
+class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+}
+
+function freshDeck() {
+  const data = SUITS.flatMap((suit) => {
+    return VALUES.map((value) => {
+      return new Card(suit, value);
+    });
+  });
+
+  const values = ['+4', 's1'];
+  for (let i = 0; i < 2; i++) {
+    values.map((value) => {
+      data.push(new Card('s', value));
+    });
+  }
+  return data;
+}
