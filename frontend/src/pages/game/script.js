b7a1a95a5fabf48cdb4378a587d4adba41210c24
ayoline <ayo@outlook.com.br>
2022-09-07 13:31:48 -0300

    add css colors, cards, reset and css class cards

--- frontend/src/pages/game/script.js
+++ frontend/src/pages/game/script.js
@@ -4,3 +4,21 @@ import Deck from '../../utils/deck.js';
 const deck = new Deck();
 deck.shuffle();
 console.log(deck.cards);
+
+// testando cards
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const randomNumber = getRandomIntInclusive(0, deck.cards.length);
+console.log(deck.cards[randomNumber]);
+const suit = deck.cards[randomNumber].suit;
+const value = deck.cards[randomNumber].value;
+
+const body = document.querySelector('body');
+const card = document.getElementById('card');
+card.classList.add(`${suit}${value}`);
+
+body.append(card);
