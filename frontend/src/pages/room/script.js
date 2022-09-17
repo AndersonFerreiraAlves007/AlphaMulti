99567165609a349fafbb4439d00e60a7b21be604
ayoline <ayo@outlook.com.br>
2022-09-17 17:49:37 -0300

    fix the waiting room page

--- frontend/src/pages/room/script.js
+++ frontend/src/pages/room/script.js
@@ -1,56 +1,118 @@
-class Room {
-  static renderRoom = () => {
-    const body = document.querySelector('body');
-    const main = document.createElement('main');
+const renderWaitingRoomPage = () => {
+  //Globals.serverCommunication.enterRadomRoom();
+  const page = document.getElementById('page');
 
-    const imgBackground = document.createElement('img');
-    imgBackground.classList.add('background');
-    imgBackground.src = '../../assets/img/background.png';
+  const imgBackground = document.createElement('img');
+  imgBackground.classList.add('background');
+  imgBackground.src = '../../assets/img/background.png';
 
-    const divButtons = document.createElement('div');
-    divButtons.classList.add('buttons__configure');
-    const btnSound = document.createElement('img');
-    btnSound.classList.add('button__sound');
-    btnSound.src = '../../assets/img/button-sound.png';
-    const btnClose = document.createElement('img');
-    btnClose.classList.add('button__close');
-    btnClose.src = '../../assets/img/button-close.png';
-    divButtons.append(btnSound, btnClose);
+  const divButtons = document.createElement('div');
+  divButtons.classList.add('buttons__configure');
 
-    const imgCards = document.createElement('img');
-    imgCards.classList.add('img__cards');
-    imgCards.src = '../../assets/img/cartas-room.png';
+  const btnSound = document.createElement('img');
+  btnSound.classList.add('button__sound');
+  btnSound.src = '../../assets/img/button-sound.png';
 
-    const imglogo = document.createElement('img');
-    imglogo.classList.add('logo');
-    imglogo.src = '../../assets/img/logo.png';
+  const btnClose = document.createElement('img');
+  btnClose.classList.add('button__close');
+  btnClose.src = '../../assets/img/button-close.png';
 
-    const title = document.createElement('p');
-    title.innerText = 'Escolha uma opção';
+  const btnBack = document.createElement('img');
+  btnBack.classList.add('button__back');
+  btnBack.src = '../../assets/img/back-icon.svg';
 
-    const divRooms = document.createElement('div');
-    divRooms.classList.add('rooms');
-    const divRoom1 = document.createElement('div');
-    const room1Title = document.createElement('p');
-    room1Title.innerText = 'Salas Aleatórias';
-    const room1Img = document.createElement('img');
-    room1Img.src = '../../assets/img/public-room.svg';
-    divRoom1.append(room1Title, room1Img);
+  divButtons.append(btnSound, btnClose, btnBack);
 
-    const divRoom2 = document.createElement('div');
-    const room2Title = document.createElement('p');
-    room2Title.innerText = 'Salas Privadas';
-    const room2Img = document.createElement('img');
-    room2Img.src = '../../assets/img/private-room.svg';
-    divRoom2.append(room2Title, room2Img);
+  const main = document.createElement('main');
+  main.classList.add('main__waiting--room');
 
-    divRooms.append(divRoom1, divRoom2);
+  const imgLogo = document.createElement('img');
+  imgLogo.classList.add('room--logo');
+  imgLogo.src = '../../assets/img/logo.png';
 
-    main.append(imglogo, title, divRooms);
-    body.append(imgBackground, divButtons, main, imgCards);
-  };
-}
+  const title = document.createElement('p');
+  title.innerText = 'Esperando os outros jogadores entrarem na sala...';
 
-//Room.renderRoom();
+  const h2 = document.createElement('h2');
+  h2.innerText = 'Entraram (3/4)';
 
-export { Room };
+  const divAllusers = document.createElement('div');
+  divAllusers.classList.add('div__users');
+
+  const divUser1 = document.createElement('div');
+  divUser1.classList.add('div_user');
+  const imgUser1 = document.createElement('img');
+  imgUser1.src = '../../assets/img/users/user2.svg';
+  const pUser1 = document.createElement('p');
+  pUser1.innerText = 'Usúario 1';
+  divUser1.append(imgUser1, pUser1);
+
+  const divUser2 = document.createElement('div');
+  divUser2.classList.add('div_user');
+  const imgUser2 = document.createElement('img');
+  imgUser2.src = '../../assets/img/users/user9.svg';
+  const pUser2 = document.createElement('p');
+  pUser2.innerText = 'Usúario 2';
+  divUser2.append(imgUser2, pUser2);
+
+  const divUser3 = document.createElement('div');
+  divUser3.classList.add('div_user');
+  const imgUser3 = document.createElement('img');
+  imgUser3.src = '../../assets/img/users/user6.svg';
+  const pUser3 = document.createElement('p');
+  pUser3.innerText = 'Usúario 3';
+  divUser3.append(imgUser3, pUser3);
+
+  const divUser4 = document.createElement('div');
+  divUser4.classList.add('div_user');
+  const imgUser4 = document.createElement('img');
+  imgUser4.src = '';
+  const pUser4 = document.createElement('p');
+  pUser4.innerText = 'Esperando jogador...';
+  divUser4.append(imgUser4, pUser4);
+
+  divAllusers.append(divUser1, divUser2, divUser3, divUser4);
+
+  const cardRoom = document.createElement('img');
+  cardRoom.classList.add('img__cards');
+  cardRoom.src = '../../assets/img/cartas-room.png';
+
+  main.append(imgLogo, title, h2, divAllusers);
+  page.append(main, cardRoom, divButtons, imgBackground);
+  /*  document.getElementById('page').innerHTML = `
+       <main>
+           <img class="cartas__room" src="../../assets/img/cartas-room.png">
+           <div class="buttons__configure">
+               <input class="button__sound" type="image" src="../../assets/img/button-sound.png">
+               <input class="button__close" type="image" src="../../assets/img/button-close.png">
+           </div>
+           <img class="backgroud" src="../../assets/img/background.png">
+           <div class="logo">
+               <img src="../../assets/img/logo.png">
+           </div>
+           <div class="infor">
+               <h1>Esperando os outros jogadores entrarem...</h1>
+               <h2>Entraram (3/4)</h2>
+               <div class="userAll">
+                   <div class="user">
+                       <img src="../../assets/img/users/user2.svg">
+                       <p>Usúario 1</p>
+                   </div>
+                   <div class="user">
+                       <img src="../../assets/img/users/user9.svg">
+                       <p>Usúario 2</p>
+                   </div>
+                   <div class="user">
+                       <img src="../../assets/img/users/user6.svg">
+                       <p>Usúario 3</p>
+                   </div>
+                   <div class="user">
+                       <p>Esperando jogador...</p>
+                   </div>
+               </div>
+           </div>
+       </main>
+     ` */
+};
+
+renderWaitingRoomPage();
--- frontend/src/utils/waitingRoomPage.js
+++ frontend/src/utils/waitingRoomPage.js
@@ -3,53 +3,47 @@ import { Globals } from './globals.js';
 const renderWaitingRoomPage = () => {
   Globals.serverCommunication.enterRadomRoom();
   const page = document.getElementById('page');
+  page.classList.add('waiting--room-page');
 
-  const main = document.createElement('main');
-  main.classList.add('room');
-
-  const cardRoom = document.createElement('img');
-  cardRoom.classList.add('cartas__room');
-  cardRoom.src = './src/assets/img/cartas-room.png';
+  const imgBackground = document.createElement('img');
+  imgBackground.classList.add('background');
+  imgBackground.src = './src/assets/img/background.png';
 
   const divButtons = document.createElement('div');
   divButtons.classList.add('buttons__configure');
 
-  const btnSound = document.createElement('input');
-  btnSound.type = 'image';
+  const btnSound = document.createElement('img');
   btnSound.classList.add('button__sound');
   btnSound.src = './src/assets/img/button-sound.png';
 
-  const btnClose = document.createElement('input');
-  btnClose.type = 'image';
+  const btnClose = document.createElement('img');
   btnClose.classList.add('button__close');
   btnClose.src = './src/assets/img/button-close.png';
 
+  // const btnBack = document.createElement('img');
+  // btnBack.classList.add('button__back');
+  // btnBack.src = './src/assets/img/back-icon.svg';
+
   divButtons.append(btnSound, btnClose);
 
-  const imgBackground = document.createElement('img');
-  imgBackground.classList.add('backgroud');
-  imgBackground.src = './src/assets/img/background.png';
+  const main = document.createElement('main');
+  main.classList.add('main__waiting--room');
 
-  const divLogo = document.createElement('div');
-  divLogo.classList.add('logo');
   const imgLogo = document.createElement('img');
+  imgLogo.classList.add('room--logo');
   imgLogo.src = './src/assets/img/logo.png';
-  divLogo.append(imgLogo);
 
-  const divInfo = document.createElement('div');
-  divInfo.classList.add('infor');
-
-  const h1 = document.createElement('h1');
-  h1.innerText = 'Esperando os outros jogadores entrarem...';
+  const title = document.createElement('p');
+  title.innerText = 'Esperando os outros jogadores entrarem na sala...';
 
   const h2 = document.createElement('h2');
   h2.innerText = 'Entraram (3/4)';
 
   const divAllusers = document.createElement('div');
-  divAllusers.classList.add('userAll');
+  divAllusers.classList.add('div__users');
 
   const divUser1 = document.createElement('div');
-  divUser1.classList.add('user');
+  divUser1.classList.add('div_user');
   const imgUser1 = document.createElement('img');
   imgUser1.src = './src/assets/img/users/user2.svg';
   const pUser1 = document.createElement('p');
@@ -57,7 +51,7 @@ const renderWaitingRoomPage = () => {
   divUser1.append(imgUser1, pUser1);
 
   const divUser2 = document.createElement('div');
-  divUser2.classList.add('user');
+  divUser2.classList.add('div_user');
   const imgUser2 = document.createElement('img');
   imgUser2.src = './src/assets/img/users/user9.svg';
   const pUser2 = document.createElement('p');
@@ -65,7 +59,7 @@ const renderWaitingRoomPage = () => {
   divUser2.append(imgUser2, pUser2);
 
   const divUser3 = document.createElement('div');
-  divUser3.classList.add('user');
+  divUser3.classList.add('div_user');
   const imgUser3 = document.createElement('img');
   imgUser3.src = './src/assets/img/users/user6.svg';
   const pUser3 = document.createElement('p');
@@ -73,55 +67,20 @@ const renderWaitingRoomPage = () => {
   divUser3.append(imgUser3, pUser3);
 
   const divUser4 = document.createElement('div');
-  divUser4.classList.add('user');
+  divUser4.classList.add('div_user');
   const imgUser4 = document.createElement('img');
   imgUser4.src = '';
   const pUser4 = document.createElement('p');
   pUser4.innerText = 'Esperando jogador...';
   divUser4.append(imgUser4, pUser4);
 
-  divUser4.addEventListener('click', () => {
-    navigate('game');
-  });
-
   divAllusers.append(divUser1, divUser2, divUser3, divUser4);
-  divInfo.append(h1, h2, divAllusers);
-  main.append(cardRoom, divButtons, imgBackground, divLogo, divInfo);
-  page.append(main);
-  /*  document.getElementById('page').innerHTML = `
-       <main>
-           <img class="cartas__room" src="./src/assets/img/cartas-room.png">
-           <div class="buttons__configure">
-               <input class="button__sound" type="image" src="./src/assets/img/button-sound.png">
-               <input class="button__close" type="image" src="./src/assets/img/button-close.png">
-           </div>
-           <img class="backgroud" src="./src/assets/img/background.png">
-           <div class="logo">
-               <img src="./src/assets/img/logo.png">
-           </div>
-           <div class="infor">
-               <h1>Esperando os outros jogadores entrarem...</h1>
-               <h2>Entraram (3/4)</h2>
-               <div class="userAll">
-                   <div class="user">
-                       <img src="./src/assets/img/users/user2.svg">
-                       <p>Usúario 1</p>
-                   </div>
-                   <div class="user">
-                       <img src="./src/assets/img/users/user9.svg">
-                       <p>Usúario 2</p>
-                   </div>
-                   <div class="user">
-                       <img src="./src/assets/img/users/user6.svg">
-                       <p>Usúario 3</p>
-                   </div>
-                   <div class="user">
-                       <p>Esperando jogador...</p>
-                   </div>
-               </div>
-           </div>
-       </main>
-     ` */
-};
 
+  const cardRoom = document.createElement('img');
+  cardRoom.classList.add('img__cards');
+  cardRoom.src = './src/assets/img/cartas-room.png';
+
+  main.append(imgLogo, title, h2, divAllusers);
+  page.append(main, cardRoom, divButtons, imgBackground);
+};
 export { renderWaitingRoomPage };
